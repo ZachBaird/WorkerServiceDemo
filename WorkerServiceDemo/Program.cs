1f4da32160e65da723c8d8b782481733d3836d4e
ZachBaird <zach.d.baird@leidos.com>
2020-09-24 16:39:16 -0400

    Add project files.

---
+++ WorkerServiceDemo/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
+
+namespace WorkerServiceDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.File(@"C:\Dev\demo\LogFile.txt")
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting up the service");
+                CreateHostBuilder(args).Build().Run();
+                return;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex.Message);
+                return;
+            }
+            finally
+            {
+                // Ensures that if there's any messages in the buffer, 
+                //  they'll get written before the app closes.
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .UseSerilog();
+    }
+}
